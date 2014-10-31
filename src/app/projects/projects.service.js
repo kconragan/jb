c82301cc63e1d092fe035e8bc8ab5f65ef097f0c
Kai Conragan <kconragan@kconragan-macbookpro3.roam.corp.google.com>
2014-10-30 19:58:24 -0700

    Add explicit size parameter to image fetches to avoid inverted rendering

--- src/app/projects/projects.service.js
+++ src/app/projects/projects.service.js
@@ -6,24 +6,47 @@ angular.module('projects', [])
   .factory('dataService', function($http, $q) {
 
     var albumsURL = 'https://picasaweb.google.com/data/feed/api/user/' + GOOGLEID + '/?alt=json';
+    
+    _createPiasaPhotoUrl= function(id, size) {
+      var size = size || 's1280';
+      var tempUrl = id.split('/');
+      var url = tempUrl[0] + '//' + tempUrl[2] + '/' + tempUrl[3] + '/' + tempUrl[4] + '/' + tempUrl[5] + '/' + tempUrl[6] + '/s1280/' + tempUrl[7];
+      console.log(url);
+      return url
+    }
 
     return {
 
+      /*
+       * getAlbums fetches all albums from Picasaweb for a given
+       * user id and returns as a list
+       */
       getAlbums : function() {
+        
+        // Create promise
         var deferred = $q.defer();
         var albums = [];
+        
+        // Send initial request
         $http.get(albumsURL).success(function(data) {
           var projects = data.feed.entry;
+          
+          // Currently sorting projects by last updated
+          // This is a hack until we can figure out a more
+          // deterministic way to establish sorting
           _.sortBy(projects, function (obj) {
             var d = new Date(obj.updated.$t);
             console.log(d.getTime());
             return d.getTime();
           }).reverse();
+          
+          // Organize each project
           _.each(projects, function(data) {
+            
             var album = {};
-            // console.log(data.feed.entry[i]);
-            album.id = data.gphoto$id.$t;
-            album.title = data.title.$t;
+            
+            album.id       = data.gphoto$id.$t;
+            album.title    = data.title.$t;
             album.modified = data.updated.$t;
             album.thumbUrl = data.link[0].href;
             
@@ -32,8 +55,11 @@ angular.module('projects', [])
               return;
             }
             
+            // Fetch cover image for the album
             $http.get(album.thumbUrl).success(function(response) {
-              album.thumb = response.feed.entry[0].content.src;
+              // album.thumb = response.feed.entry[0].content.src;
+              album.thumb = _createPiasaPhotoUrl(response.feed.entry[0].content.src);
+              console.log('thumb url is ', album.thumb);
               albums.push(album);
             })
             .error(function(e) {
@@ -60,7 +86,7 @@ angular.module('projects', [])
         $http.get(albumURL).success(function(data) {
           console.log(data);
           project.title = data.feed.title.$t;
-          project.masthead = data.feed.entry[0].content.src;
+          project.masthead = _createPiasaPhotoUrl(data.feed.entry[0].content.src);
           var photos = [];
           for (var i = 0; i < data.feed.entry.length; i++) {
             
@@ -76,8 +102,8 @@ angular.module('projects', [])
             
             var tempUrl = item.content.src.split('/');
 
-            var url = tempUrl[0] + '//' + tempUrl[2] + '/' + tempUrl[3] + '/' + tempUrl[4] + '/' + tempUrl[5] + '/' + tempUrl[6] + '/w400-h400-c/' + tempUrl[7];
-            var bigUrl = tempUrl[0] + '//' + tempUrl[2] + '/' + tempUrl[3] + '/' + tempUrl[4] + '/' + tempUrl[5] + '/' + tempUrl[6] + '/s1280/' + tempUrl[7];
+            var url = tempUrl[0] + '//' + tempUrl[2] + '/' + tempUrl[3] + '/' + tempUrl[4] + '/' + tempUrl[5] + '/' + tempUrl[6] + '/w1200-h1200-c/' + tempUrl[7];
+            var bigUrl = tempUrl[0] + '//' + tempUrl[2] + '/' + tempUrl[3] + '/' + tempUrl[4] + '/' + tempUrl[5] + '/' + tempUrl[6] + '/s1640/' + tempUrl[7];
             item.url = url;
             item.bigUrl = bigUrl;
             
